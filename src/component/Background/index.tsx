ea200a0eb40bb7cc0e0cfc68328c8283d96aef35
franciscowxp <franciscowxp@gmail.com>
2022-10-05 21:05:24 +0800

    feat: use color.js to pick color

--- src/component/Background/index.tsx
+++ src/component/Background/index.tsx
@@ -1,11 +1,11 @@
 import React, { useCallback, useRef } from 'react';
 import { useRecoilState } from 'recoil';
-import ColorThief from 'colorthief';
+import { prominent } from 'color.js';
 
 import { colorSetState } from '@/store';
 import { CSSVariable } from '@/utils/cssVariable';
-import { closeSplashscreen } from '@/utils/action';
 import { PLATFORM } from '@/env';
+import { closeSplashscreen } from '@/utils/action';
 
 import bg0 from '@/image/bg0.jpg';
 import bg1 from '@/image/bg1.jpg';
@@ -47,24 +47,25 @@ export default React.memo(() => {
   const [, setColor] = useRecoilState(colorSetState);
   const img = useRef(null as unknown as HTMLImageElement);
   const onLoad = useCallback(async () => {
-    const colorThief = new ColorThief();
-    const palette = colorThief.getPalette(img.current, 2);
-    const vibrantColor = palette[0];
-    const mutedColor = palette[1];
+    const colors = await prominent(img.current, {
+      format: 'hex',
+      amount: 2,
+    });
+    const vibrantColor = colors[0] as string;
+    const mutedColor = colors[1] as string;
     if (vibrantColor && mutedColor) {
-      CSSVariable.setValue(
-        '--themeBaseColor',
-        `rgb(${vibrantColor.join(',')})`
-      );
-      CSSVariable.setValue('--themeMutedColor', `rgb(${mutedColor.join(',')})`);
+      CSSVariable.setValue('--themeBaseColor', vibrantColor);
+      CSSVariable.setValue('--themeMutedColor', mutedColor);
       setColor({
         mute: mutedColor,
         vibrant: vibrantColor,
       });
     }
-    if (PLATFORM === 'tauri') {
-      closeSplashscreen();
-    }
+    setTimeout(() => {
+      if (PLATFORM === 'tauri') {
+        closeSplashscreen();
+      }
+    }, 50);
   }, [setColor]);
 
   return (
